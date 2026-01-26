4cd35a55d7f13d3bb7aab09da3606a71b01a450f
abduznik <85239936+abduznik@users.noreply.github.com>
2026-01-26 19:10:40 +0200

    fix: remove base path for proper docker routing and bump to 0.3.1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/filament-tracker/',
   define: {
     'import.meta.env.VITE_USE_SQLITE': JSON.stringify(process.env.VITE_USE_SQLITE)
   }
